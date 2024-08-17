5313753eeabbba06c2cfe73ee99da77171d9ee8c
Kenneth <kennethsv10@gmail.com>
2024-08-17 16:54:39 -0600

    initial commit

---
+++ src/main/java/com/TMJ/apirest/TrackMyJob/TrackMyJobApplication.java
@@ -0,0 +1,13 @@
+package com.TMJ.apirest.TrackMyJob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackMyJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackMyJobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TMJ/apirest/TrackMyJob/TrackMyJobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TMJ.apirest.TrackMyJob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackMyJobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
